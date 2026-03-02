dd674999804e3a9eb905f7c7abc94ac0185f338e
bkv25 <brijesh@valere.io>
2026-03-02 16:20:55 +0530

    fix:first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { Suspense } from "react";
+import { useRoutes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { routes } from "./routes";
+import ThemeProvider from "./components/ThemeProvider/index.tsx";
+import GlobalLoader from "./components/GlobalLoader/index.tsx";
+
+function App() {
+  const RouteLayout = () => {
+    const element = useRoutes(routes());
+    return element;
+  };
+  return (
+    <>
+      <ThemeProvider>
+        <GlobalLoader />
+        <ToastContainer theme="dark" />
+        <Suspense
+          fallback={
+            <>
+              <div className="loader-wrapper  position-fixed w-100 h-100 top-0 start-0 d-flex justify-content-center align-items-center">
+                <span className="loader" />
+              </div>
+            </>
+          }
+        >
+          <RouteLayout />
+        </Suspense>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+const config = {
+  NAME_KEY: import.meta.env.VITE_API_APP_NAME,
+  USER_IMAGE_URL: import.meta.env.VITE_API_IMAGE_BASE_URL,
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  BACKEND_IMAGE_URL: import.meta.env.VITE_API_AWS_BASE_URL,
+  GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  GOOGLE_MAP_KEY: import.meta.env.VITE_GOOGLE_MAP_KEY,
+  AI_API_BASE_URL: import.meta.env.VITE_AI_API_BASE_URL,
+  // Use Vite's mode as a stand‑in for NODE_ENV
+  NODE_ENV: import.meta.env.MODE,
+};
+
+export default config;
