a86221e575c505ec473f6a2c12903a3279ca9104
bkv25 <brijesh@valere.io>
2026-03-02 16:47:14 +0530

    fix:mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const generateSourcemap = env.GENERATE_SOURCEMAP === 'true'
+
+  return {
+    plugins: [react()],
+    build: {
+      sourcemap: generateSourcemap,
+    },
+  }
 })
